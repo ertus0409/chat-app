904efe5ddce52711055d391e988b7f53af4c5984
ertus0409 <ertus0409@hotmail.com>
2018-09-03 18:55:55 -0400

    Added css

--- public/js/index.js
+++ public/js/index.js
@@ -21,12 +21,13 @@ socket.on('newMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   //Prevent default setting(page reloads when send tapped)
   e.preventDefault();
+  var messageTextbox = jQuery('[name=message]')
   //Collecting data from index.html and sending it to server
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('')
   });
 });
 
@@ -48,7 +49,10 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser')
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
