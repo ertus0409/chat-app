1c6b36d29c7fe02a05d86bcea947783ed610ad16
ertus0409 <ertus0409@hotmail.com>
2018-09-08 14:09:17 -0400

    Scroll to bottom if user is close to bottom.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 // const moment = require('moment');
 var socket = io();
 
+function scrollToBottom() {
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -20,7 +37,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html)
-
+  scrollToBottom();
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
   // jQuery('#messages').append(li);
@@ -57,6 +74,7 @@ socket.on('newLocationMessage', function (message) {
   // a.attr('href', message.url);
   // li.append(a);
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 //Sending data to server from location button
